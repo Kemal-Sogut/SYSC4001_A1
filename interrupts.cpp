95762adf32119cf7f0de477a5eaa0526434ee41b
Shivaani Thyagarajah <shivaani.thyagarajah@gmail.com>
2025-10-05 14:52:40 -0400

    Got rid of variables x and y, changed delay_remain var name

--- interrupts.cpp
+++ interrupts.cpp
@@ -19,16 +19,17 @@ int main(int argc, char** argv) {
     std::string execution;  //!< string to accumulate the execution output
 
     /******************ADD YOUR VARIABLES HERE*************************/
-    int x;
-    int y;
-
+    
+    std::string execution_temp; // Temporary variable to store execution line
+    int t_save = 10; // Context save/restore time
+    int t_isr_step = 40; // ISR activity time
     std::string current_isr = ""; // Current ISR being executed
     float current_time = 0; // Current time in the simulation
     int device_number = -1; // Device number for the current interrupt
     float device_duration = 0; // Duration of the current interrupt
     int hex_address = 0; // Hex address of the current ISR
     bool isr_executing = false; // Flag to indicate if an ISR is currently executing
-    int context_time = 10; //Save or restore context time changes from 10, to 20, to 30 ms
+    int delay_remain; // Value of delay corresponding with inputted device number, which will be subtracted from for each ISR step
 
 
     /******************************************************************/
@@ -46,27 +47,39 @@ int main(int argc, char** argv) {
             current_time += duration_intr; // Update current time
 
         }
+        
         else if (activity == "SYSCALL"){
-            //Simulate activity
+            delay_remain = delays.at(duration_intr);
 
-            std::tie(execution, current_time) = intr_boilerplate(current_time, duration_intr, context_time, vectors);
-            
+            //Simulate activity
+            std::tie(execution_temp, current_time) = intr_boilerplate(current_time, duration_intr, t_save, vectors);
+            execution += execution_temp + std::to_string(current_time) + ", " + std::to_string(t_isr_step) + ", Read" + "\n";
+            current_time += t_isr_step; // Update current time
+            delay_remain -= t_isr_step;
+            execution += std::to_string(current_time) + ", " + std::to_string(t_isr_step) + ", Transfer" + "\n";
+            current_time += t_isr_step; // Update current time
+            delay_remain -= t_isr_step;
+
+            execution += std::to_string(current_time) + ", " + std::to_string(delay_remain) + ", Checking errors" + "\n";
+            current_time += delay_remain; // Update current time
+
+            execution += std::to_string(current_time) + ", 1" + ", IRET\n";
+            current_time++; // Update current time
         }
+
         else if (activity == "END_IO"){
             //Simulate END_IO activity
-            execution += std::to_string(current_time) + ", " + std::to_string(duration_intr) + ", " + activity + "\n";
-            current_time += duration_intr; // Update current time
+            execution += std::to_string(current_time) + ", " + std::to_string(delays.at(duration_intr)) + ", Store information in memory" + "\n";
+            current_time += delays.at(duration_intr); // Update current time
+            execution += std::to_string(current_time) + ", 1" + ", IRET\n";
+            current_time++; // Update current time
 
         }
+
         else {
-            std::cerr << "Error: Unknown activity type: " << activity << std::endl;  
+             //Output error message
+            std::cerr << "Error: Unknown activity type: " << activity << std::endl;
         }
-
-        //print time, device number
-        //split the duration into 4 steps for your ISR
-            //call device driver
-            //
-
         /************************************************************************/
 
 
